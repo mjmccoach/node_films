73b98b7c0566d4103c13bfec9f64c95249ba2908
Matthew <m.mccoach88@gmail.com>
2021-02-24 17:20:34 +0000

    Written a function to map film titles to a new array and passes test of getting only titles from all film objects

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,10 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.filmTitles = function() {
+  return this.films.map((film) => {
+  return film.title; 
+  })
+}
+
 module.exports = Cinema;

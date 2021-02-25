6e08742206014aa7695d290282680e94e03b293d
Matthew <m.mccoach88@gmail.com>
2021-02-25 23:41:32 +0000

    Written test for filtering films by genre

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -37,10 +37,15 @@ describe('Cinema', function () {
   it('should be able to find a film by title', function() {
     const actual = cinema.getFilmByTitle("Dunkirk");
     const expected = dunkirk
-    assert.deepStrictEqual(actual, dunkirk);
+    assert.deepStrictEqual(actual, expected);
   });
 
-  it('should be able to filter films by genre');
+  it('should be able to filter films by genre', function() {
+    const actual = cinema.getFilmsByGenre("drama");
+    const expected = ["Moonlight", "T2 Trainspotting"]
+    assert.deepStrictEqual(actual, expected);
+  });
+  
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
   it('should be able to check whether all films are over a particular length');

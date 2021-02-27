22a08fb85e86026db377b7aaaabf24e6f24cc618
Matthew <m.mccoach88@gmail.com>
2021-02-27 14:46:08 +0000

    Written test for should be able to check whether there are no films a particular year

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -52,7 +52,11 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, expected)
   });
 
-  it('should be able to check whether there are no films from a particular year');
+  it('should be able to check whether there are no films from a particular year', function() {
+    const actual = cinema.getFilmsByYear(2000);
+    assert.deepStrictEqual(actual, [])
+  });
+  
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
 
